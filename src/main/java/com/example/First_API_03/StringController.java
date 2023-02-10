0889a4f99702c918ceb64b798c14c9542937266b
Fabio Frattarelli <fabiofrt@outlook.it>
2023-02-10 09:03:28 +0100

    commit

--- src/main/java/com/example/First_API_03/StringController.java
+++ src/main/java/com/example/First_API_03/StringController.java
@@ -1,10 +1,12 @@
 package com.example.First_API_03;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("string")
 public class StringController {
 
     @GetMapping(value = "/")
